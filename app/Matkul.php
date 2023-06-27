e8acaef4b3981cc86c3537b5f5382bc8bfbd72ca
2172011WilliamWijaya <90763124+WilliamWijaya217201@users.noreply.github.com>
2023-06-27 22:47:12 +0700

    Update Matkul.php

--- app/Matkul.php
+++ app/Matkul.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Matkul extends Model
 {
     protected $table = 'mata_kuliah';
-
-    protected $primaryKey = 'kode_matkul';
     protected $fillable = [
         'kode_matkul',
         'kode_prodi',
